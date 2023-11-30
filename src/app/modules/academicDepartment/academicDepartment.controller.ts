7cbc9beda819332f2e09cd1dadaef3da332b0e61
Kamrul Hasan <kamrul444216@gmail.com>
2023-12-01 03:05:20 +0600

    done all academic department routes

---
+++ src/app/modules/academicDepartment/academicDepartment.controller.ts
@@ -0,0 +1,69 @@
+import httpStatus from 'http-status';
+import sendResponse from '../../utils/sendResponse';
+import catchAsync from '../../utils/catchAsync';
+import { AcademicDepartmentService } from './academicDepartment.service';
+
+const createAcademicDepartment = catchAsync(async (req, res) => {
+  const departmentData = req.body;
+
+  const result =
+    await AcademicDepartmentService.createAcademicDepartmentIntoDB(
+      departmentData,
+    );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Department is created successfully',
+    data: result,
+  });
+});
+
+const getAllAcademicDepartment = catchAsync(async (req, res) => {
+  const result =
+    await AcademicDepartmentService.getAllAcademicDepartmentFromDB();
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Department list retrieved successfully',
+    data: result,
+  });
+});
+
+const getSingleAcademicDepartment = catchAsync(async (req, res) => {
+  const { departmentId } = req.params;
+
+  const result =
+    await AcademicDepartmentService.getSingleAcademicDepartmentFromDB(
+      departmentId,
+    );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Department retrieved successfully',
+    data: result,
+  });
+});
+
+const updateAcademicDepartment = catchAsync(async (req, res) => {
+  const { departmentId } = req.params;
+  const departmentData = req.body;
+
+  const result = await AcademicDepartmentService.updateAcademicDepartmentIntoDB(
+    departmentId,
+    departmentData,
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Department updated successfully',
+    data: result,
+  });
+});
+
+export const AcademicDepartmentController = {
+  createAcademicDepartment,
+  getAllAcademicDepartment,
+  getSingleAcademicDepartment,
+  updateAcademicDepartment,
+};
