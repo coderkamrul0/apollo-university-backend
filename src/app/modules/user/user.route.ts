10b762c8a38fc2746bf9fc28ec59347d0263b376
Kamrul Hasan <kamrul444216@gmail.com>
2023-12-08 16:34:28 +0600

    faculty route

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,12 +2,20 @@ import express from 'express';
 import { UserController } from './user.controller';
 import { studentValidations } from '../student/student.validation';
 import validateRequest from '../../middlewares/validateRequest';
+import {
+  facultyValidation,
+} from '../faculty/faculty.validation';
 const router = express.Router();
 
 router.post(
   '/create-student',
   validateRequest(studentValidations.createStudentValidationSchema),
   UserController.createStudent,
+);
+router.post(
+  '/create-faculty',
+  validateRequest(facultyValidation.createFacultyValidationSchema),
+  UserController.createFaculty,
 );
 
 export const userRoutes = router;
